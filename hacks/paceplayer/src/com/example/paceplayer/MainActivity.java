f92f803adeb481aa56e20c70f350cb8cef8e618f
trisha <kotharitrisha@gmail.com>
2014-03-04 09:13:55 -0500

    Paceplayer

---
+++ hacks/paceplayer/src/com/example/paceplayer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.paceplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button button = (Button) findViewById(R.id.play_song);
+		/*button.setOnClickListener(null) {
+			public View {
+				
+			}
+			
+		};*/
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
